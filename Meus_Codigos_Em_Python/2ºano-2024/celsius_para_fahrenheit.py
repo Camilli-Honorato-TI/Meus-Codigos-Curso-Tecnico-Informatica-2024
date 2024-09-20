08a88a8f2dc0c3a0747eff60011716f1b16a826e
Camilli Honorato <camillihonoratovit@gmail.com>
2024-09-20 11:32:36 -0300

    Add files via upload

---
+++ Meus_Codigos_Em_Python/2ºano-2024/celsius_para_fahrenheit.py
@@ -0,0 +1,10 @@
+#Camilli Honorato
+#2-53 - Cedup - Cedup Curso Técnico em Informática
+#Feito em: 20/09/2024
+
+C = float(input("Informe a temperatura em Cº:"))
+
+def celsius_para_fahrenheit():
+    return (C * 1.8) + 32
+
+print("A temperatura em Fº é de: %.2f" % celsius_para_fahrenheit())
