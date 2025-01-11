985775443425c4028c349eddc44cff6a95c837ce
Camilli Honorato <camillihonoratovit@gmail.com>
2025-01-11 16:49:39 -0300

    Delete Meus-Codigos-Java/Lógica de Programação directory

--- Meus-Codigos-Java/Lógica de Programação/olamundo14_05_24.java
+++
@@ -1,9 +0,0 @@
-//Primeiro programa feito em Java na aula de lógica de Programação
-//Autor: Camilli Honorato
-
-public class olamundo14_05_24{//Nome do programa, e com a data do dia feito
-
-    public static void main(String[] args){
-        System.out.print("Olá Mundo!"); //Mostra a mensagem "Olá Mundo!", apenas o que está dentro do paraentesis, na tela do usuario
-    }
-}
