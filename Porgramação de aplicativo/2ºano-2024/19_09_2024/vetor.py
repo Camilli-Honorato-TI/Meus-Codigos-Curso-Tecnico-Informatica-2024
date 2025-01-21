300c936c0f5a44957572f3f7cd243e7537e540c8
Camilli Honorato <camillihonoratovit@gmail.com>
2025-01-21 19:43:27 -0300

    Delete Porgramação de aplicativo directory

--- Porgramação de aplicativo/2ºano-2024/19_09_2024/vetor.py
+++
@@ -1,8 +0,0 @@
-#Camilli Honorato
-#2-53 - Cedup - Cedup Curso Técnico em Informática
-#Feito em: 19/09/2024
-
-nomes = ["Edilson", "Murilo", "Lucius",  "Yan", "Bruno"]
-
-for i in nomes:
-    print("Devedor: ", i)
--- Porgramação de aplicativo/2ºano-2024/vetor.py
+++
@@ -1,7 +0,0 @@
-#Camilli Vitória Salvaro Honorato
-#2-53 - Cedup - Cursando Curso Técnico em Informática
-
-nomes = ["Edilson", "Murilo", "Lucius",  "Yan", "Bruno"]
-
-for i in nomes:
-    print("Devedor: ", i)
